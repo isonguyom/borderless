cd58b9e23f8c0edc050e3987b89e7a0fd8d3d483
Martin Isonguyo <52254145+isonguyom@users.noreply.github.com>
2025-09-19 20:27:00 +0100

    Update SwapForm.vue

--- src/components/actions/SwapForm.vue
+++ src/components/actions/SwapForm.vue
@@ -1,8 +1,8 @@
 <script setup>
 import { ref } from 'vue'
-import { useWalletStore } from '@/stores/wallets'
+import { useWalletsStore } from '@/stores/wallets'
 
-const walletStore = useWalletStore()
+const walletStore = useWalletsStore()
 const fromWalletId = ref(null)
 const toWalletId = ref(null)
 const amount = ref(0)
