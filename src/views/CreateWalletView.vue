06bc8db3c4957aa068338ca95bcffe70e446b8bc
Martin Isonguyo <52254145+isonguyom@users.noreply.github.com>
2025-09-21 02:35:29 +0100

    Update CreateWalletView.vue

--- src/views/CreateWalletView.vue
+++ src/views/CreateWalletView.vue
@@ -1,12 +1,12 @@
 <script setup>
 import AppLayout from '@/layouts/AppLayout.vue'
-import CreateWalletForm from '@/components/actions/CreateWalletForm.vue'
+
 </script>
 
 <template>
   <AppLayout>
     <div class="max-w-lg mx-auto">
-      <CreateWalletForm />
+      Create 
     </div>
   </AppLayout>
 </template>
