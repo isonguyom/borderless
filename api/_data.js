b8dc228ce026ca0c582b08022065d54ce0770fda
isonguyom <isonguyom@gmail.com>
2025-09-21 22:17:43 +0100

    Add supported currencies

--- api/_data.js
+++ api/_data.js
@@ -4,7 +4,14 @@ let db = {
     { value: "NGN", label: "Nigerian Naira", symbol: "₦", type: "fiat", color: "#008000", rate: 1500 },
     { value: "EUR", label: "Euro", symbol: "€", type: "fiat", color: "#003399", rate: 0.92 },
     { value: "GBP", label: "British Pound", symbol: "£", type: "fiat", color: "#990000", rate: 0.8 },
+
+    // Stablecoins
+    { value: "USDT", label: "Tether", symbol: "₮", type: "stablecoin", color: "#26A17B", rate: 1 },
+    { value: "USDC", label: "USD Coin", symbol: "Ⓢ", type: "stablecoin", color: "#2775CA", rate: 1 },
+    { value: "BUSD", label: "Binance USD", symbol: "Ⓑ", type: "stablecoin", color: "#F3BA2F", rate: 1 },
+    { value: "DAI", label: "Dai", symbol: "Đ", type: "stablecoin", color: "#F5AC37", rate: 1 }
   ],
+
   users: [],
   tokens: [],
   wallets: [],
