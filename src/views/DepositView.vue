ed005afc488cb06cd1e1ab29bf558c1643149a17
Martin Isonguyo <52254145+isonguyom@users.noreply.github.com>
2025-09-21 02:36:21 +0100

    Update DepositView.vue

--- src/views/DepositView.vue
+++ src/views/DepositView.vue
@@ -1,12 +1,11 @@
 <script setup>
 import AppLayout from '@/layouts/AppLayout.vue'
-import DepositForm from '@/components/actions/DepositForm.vue'
 </script>
 
 <template>
   <AppLayout>
     <div class="max-w-lg mx-auto">
-      <DepositForm />
+      Deposit 
     </div>
   </AppLayout>
 </template>
